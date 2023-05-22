2ee9d8b59c83d3e3c5f5d7e84226bacdd11c9640
JuliDaForno <jdaforno@udesa.edu.ar>
2023-05-22 17:59:20 -0300

    proyecto base

---
+++ my-app/firebase/config.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtOKuFru1K4y-V67K2rk_X4LdctPI5aWE",
+    authDomain: "proyectointegrador-cf011.firebaseapp.com",
+    projectId: "proyectointegrador-cf011",
+    storageBucket: "proyectointegrador-cf011.appspot.com",
+    messagingSenderId: "858195969626",
+    appId: "1:858195969626:web:2124b56e6e14635a747a63",
+    measurementId: "G-6NPG0332PZ"
+  };
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+export const storage = app.storage()
+export const db = app.firestore()
