ae39d37d24c6957c0e32c8abca2682024330e7a6
solapellaniz <sdeapellaniz@udesa.edu.ar>
2023-06-03 13:26:45 -0300

    creando posts

--- my-app/src/screens/NewPost.js
+++ my-app/src/screens/NewPost.js
@@ -1,14 +1,67 @@
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
+import FormPost from '../components/FormPost'
+import MyCamera from '../components/MyCamera'
+
+import {db, auth} from '../firebase/config'
+
 
 class NewPost extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      descripcion: '',
+      foto: '',
+      likes: [],
+      comments: []
+    }
+  }
+
+  actualizarDescripcion(text){
+    this.setState({
+      descripcion: text
+    })
+  }
+
+  crearPosteo({descripcion, foto, likes, comments}){
+    db.collection('posts').add({
+      owner: auth.currentUser.email,
+      descripcion: descripcion,
+      foto: foto,
+      createdAt: Date.now(),
+      likes: likes,
+      comments: comments,
+    })
+    .then((resp)=> {
+      this.props.navigation.navigate('Home')
+    })
+    .catch(err => console.log(err))
+
+  }
+
   render() {
     return (
       <View>
-        <Text>NewPost</Text>
+       <FormPost stateDescripcion={this.state.descripcion} actualizarDescripcion= {(text) => this.actualizarDescripcion(text)} />
+       <TouchableOpacity
+       onPress= {()=> this.crearPosteo({
+        descripcion: this.state.descripcion,
+        foto: this.state.foto,
+        likes: this.state.comments
+       })}
+       >
+
+       </TouchableOpacity>
       </View>
     )
   }
 }
 
-export default  NewPost
+export default NewPost
+
+/*         {
+          this.state.foto === '' ?
+            <MyCamera />
+            :
+            <Text>NewPost</Text>
+        } */
