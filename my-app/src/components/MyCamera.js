4bf3bce432acbb6390e1f275fdab59d41f72688d
solapellaniz <sdeapellaniz@udesa.edu.ar>
2023-06-03 18:13:23 -0300

    camara

--- my-app/src/components/MyCamera.js
+++ my-app/src/components/MyCamera.js
@@ -1,18 +1,43 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
+import {Camera} from 'expo-camera'
 
 class MyCamera extends Component {
     constructor (props){
         super(props)
+        this.state= {
+          mostrarCamara: false,
+          fotoTomada: ''
+        }
+        this.metodosCamara 
+    }
+  
+    componentDidMount(){
+      Camera.getCameraPermissionsAsync()
+      .then(resp => this.setState({mostrarCamara: true}))
+      .catch(err=> console.log(err))
     }
     
   render() {
     return (
-      <View>
-        <Text>MyCamera</Text>
+      <View style={styles.container}>
+        <Camera
+        style={styles.camara}
+        type= {Camera.Constants.Type.back}
+        ref={(metodosComponente)=>this.metodosCamara = metodosComponente}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  camara: {
+    height: 250
+  }
+})
 
 export default MyCamera
