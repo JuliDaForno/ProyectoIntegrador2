fff1fa952118ee09cbc22f09be1465a6c4351a34
solapellaniz <sdeapellaniz@udesa.edu.ar>
2023-06-05 15:28:15 -0300

    merges

--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,98 +1,101 @@
 import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native'
-import {FontAwesome} from '@expo/vector-icons'
+import { FontAwesome } from '@expo/vector-icons'
 import React, { Component } from 'react'
 import { db, auth } from '../firebase/config'
-import firebase from 'firebase'  
+import firebase from 'firebase'
 
 
 class Post extends Component {
-    constructor(props){
+  constructor(props) {
     super(props)
-        this.state={
-            isLiked: false,
-            owner: false
-        
-           
-        }
-    }
-    
-    componentDidMount(){
-        let estaMiLike = this.props.data.data.likes.includes(auth.currentUser.email)
-        if(estaMiLike ===true){
-            this.setState({
-                isLiked: true
-            })
-        }
-    }
+    this.state = {
+      isLiked: false,
+      owner: false
 
-  like(){
-    db.collection('posts')
-    .doc(this.props.data.id)
-    .update({
-      likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
-    })
-    .then((resp) =>{
+
+    }
+  }
+
+  componentDidMount() {
+    let estaMiLike = this.props.data.data.likes.includes(auth.currentUser.email)
+    if (estaMiLike === true) {
       this.setState({
-        isLiked:true
+        isLiked: true
       })
-    })
-    .catch(err => console.log(err))
+    }
+  }
+
+  like() {
+    db.collection('posts')
+      .doc(this.props.data.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+      })
+      .then((resp) => {
+        this.setState({
+          isLiked: true
+        })
+      })
+      .catch(err => console.log(err))
 
   }
 
-  unlike(){
+  unlike() {
     db.collection('posts').doc(this.props.data.id)
-    .update({
-      likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
-    })
-    .then((resp) => this.setState({
-      isLiked:false
-    }))
-    .catch(err => console.log(err))
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+      })
+      .then((resp) => this.setState({
+        isLiked: false
+      }))
+      .catch(err => console.log(err))
 
-    
+
   }
-
-
 
   render() {
     console.log('props desde post')
     console.log(this.props)
     return (
-      <View>
-        <Image 
-        source= {{uri: this.props.data.data.foto}}
-        style= {styles.img}
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('ProfileUsers', { email: this.props.data.data.owner })}>
+          <Text>{this.props.data.data.owner}</Text>
+        </TouchableOpacity>
+
+        <Image
+          source={{ uri: this.props.data.data.foto }}
+          style={styles.img}
         />
+
         <Text>Post</Text>
 
         <Text>{this.props.data.data.descripcion}</Text>
         {console.log(this.props.data.data)}
         {
           this.state.isLiked ?
-          <TouchableOpacity
-          onPress={()=> this.unlike()}
-          >
-            <FontAwesome
-            name='heart'
-            size={24}
-            color='red'
-            />
-          </TouchableOpacity>
-          :
-          <TouchableOpacity
-          onPress={()=> this.like()}
-          >
-            <FontAwesome
-            name='heart-o'
-            size={24}
-            color='red'
-            />
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.unlike()}
+            >
+              <FontAwesome
+                name='heart'
+                size={24}
+                color='red'
+              />
+            </TouchableOpacity>
+            :
+            <TouchableOpacity
+              onPress={() => this.like()}
+            >
+              <FontAwesome
+                name='heart-o'
+                size={24}
+                color='red'
+              />
+            </TouchableOpacity>
         }
         <View>
           <TouchableOpacity
-          onPress= {()=> this.props.navigation.navigate('Comments')}
+            onPress={() => this.props.navigation.navigate('Comments')}
           >
             <Text>
               Agregar comentario
@@ -103,15 +106,16 @@ class Post extends Component {
     )
   }
 }
-  
+
 const styles = StyleSheet.create({
-  container:{
+  container: {
     marginVertical: 15
   },
   img: {
     height: 200
   }
 })
-  
+
+
 export default Post
 
